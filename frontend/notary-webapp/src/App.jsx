3f6fcc4684c67bee0e0902536024169b45ea73dc
MB <michal@web3devs.com>
2022-10-19 14:45:39 +0200

    Further project setup

--- frontend/notary-webapp/src/App.jsx
+++ frontend/notary-webapp/src/App.jsx
@@ -1,19 +1,40 @@
-import { useState } from 'react';
-import { Button } from 'primereact/Button';
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; //theme
-import 'primereact/resources/primereact.min.css'; //core css
-import 'primeicons/primeicons.css'; //icons
+import React, { useState } from 'react'
+import {
+  NearEnvironment,
+  NearEnvironmentProvider,
+  NearProvider
+} from 'react-near'
 
-import './App.scss';
+import 'primereact/resources/themes/lara-light-indigo/theme.css' //theme
+import 'primereact/resources/primereact.min.css' //core css
+import 'primeicons/primeicons.css' //icons
+
+import './App.scss'
+import HomePage from './pages/Home.page'
+import SignInPage from './pages/SignIn.page'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />
+  },
+  {
+    path: '/sign-in',
+    element: <SignInPage />
+  }
+])
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Button label="test" />
-    </div>
-  );
+    <NearEnvironmentProvider defaultEnvironment={NearEnvironment.TestNet}>
+      <NearProvider authContractId="my-contract.testnet">
+        <RouterProvider router={router} />
+      </NearProvider>
+    </NearEnvironmentProvider>
+  )
 }
 
-export default App;
+export default App
--- frontend/notary-webapp/src/main.jsx
+++ frontend/notary-webapp/src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.scss';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+import { Buffer } from 'buffer'
+
+window.Buffer = Buffer
+
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
