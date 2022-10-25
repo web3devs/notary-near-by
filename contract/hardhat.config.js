3c356fbcbc3572fff0d88e659f707aa0c0e2cdce
bgok <ken@citizenhodler.com>
2022-10-25 06:32:50 -0700

    Artifacts needed to run the aurora localhost developer node.

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -5,17 +5,21 @@ require("./tasks/totalSupply");
 require("./tasks/balanceOf");
 require("./tasks/approve");
 require("./tasks/transferFrom");
-// Replace this private key with your Ropsten account private key
-// To export your private key from Metamask, open Metamask and
-// go to Account Details > Export Private Key
-// Be aware of NEVER putting real Ether into testing accounts
 require('dotenv').config();
 
 const AURORA_PRIVATE_KEY = process.env.AURORA_PRIVATE_KEY;
+const AURORA_API_KEY = process.env.AURORA_API_KEY;
 
 module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "local_aurora",
   networks: {
+    aurora: {
+      url: `https://mainnet.aurora.dev/${AURORA_API_KEY}`,
+      accounts: [`0x${AURORA_PRIVATE_KEY}`],
+      chainId: 1313161554,
+      gasPrice: 120 * 1000000000
+    },
     testnet_aurora: {
       url: 'https://testnet.aurora.dev',
       accounts: [`0x${AURORA_PRIVATE_KEY}`],
@@ -25,16 +29,8 @@ module.exports = {
     local_aurora: {
       url: 'http://localhost:8545',
       accounts: [`0x${AURORA_PRIVATE_KEY}`],
-      chainId: 1313161555,
+      chainId: 31337,
       gasPrice: 120 * 1000000000
-    },
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [`0x${AURORA_PRIVATE_KEY}`],
-      chainId: 3,
-      live: true,
-      gasPrice: 50000000000,
-      gasMultiplier: 2,
     },
   }
 };
