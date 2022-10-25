1fd9eefcb131e736187a153dd1b8eb68d1346fbb
MB <michal@web3devs.com>
2022-10-25 08:49:25 +0200

    Added mock methods to contracts

--- frontend/notary-webapp/src/contracts/index.js
+++ frontend/notary-webapp/src/contracts/index.js
@@ -2,9 +2,36 @@ export const signUpNotary = (companyName, notaryLicenseFile, stampFile) => {
   // add actual contract interaction here
   return new Promise((res, rej) => {
     setTimeout(() => {
+      tmpNotaryState = { ...tmpNotaryState, isNotary: true }
       res()
     }, 3000)
   })
+}
+
+export const signUpClient = (form, idFile) => {
+  // add actual contract interaction here
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      tmpClientState = { ...tmpClientState, isClient: true }
+      res()
+    }, 3000)
+  })
+}
+
+// mock data
+let tmpClientState = {
+  isClient: false
+}
+let tmpNotaryState = {
+  isNotary: false
+}
+
+export const getNotaryAccount = (accountAddress) => {
+  return tmpNotaryState
+}
+
+export const getClientAccount = (accountAddress) => {
+  return tmpClientState
 }
 
 export const getOrders = () => {
