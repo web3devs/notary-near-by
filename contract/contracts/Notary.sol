4a69d7fa71ed96aa2c467f6353ead6cd631375d5
Ken Hodler <bgok@users.noreply.github.com>
2022-10-27 16:19:08 -0700

    Split payments between the notary and the service (#2)
    * Add payment splitting to the contract
    * Add information about the latest deployment to the README
    
    Co-authored-by: bgok <ken@citizenhodler.com>

--- contract/contracts/Notary.sol
+++ contract/contracts/Notary.sol
@@ -5,29 +5,55 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./NotarizedDocumentNft.sol";
 
 struct UnmintedTokenData {
+    address notary;
     address authorizedMinter;
     uint256 costToMint;
 }
 
 contract Notary is AccessControl {
-    NotarizedDocumentNft public ndn;
+    NotarizedDocumentNft public notarizedDocumentNftContract;
 
     bytes32 public constant NOTARY_ROLE = keccak256("NOTARY_ROLE");
     mapping(string => UnmintedTokenData) public unmintedToken;
+    address public commissionPayee;
+    uint256 public defaultCommissionPercentage;
+    mapping(address => uint256) public balance;
 
     event NotarizedDocumentCreated(address notary, address authorizedMinter, uint256 price, string metadataUrl);
     event NotarizedDocumentNftMinted(address authorizedMinter, uint256 price, string metadataUrl, uint256 tokenId);
+    event CommissionPayeeChanged(address commissionPayee);
+    event BalanceIncreased(address payee, uint256 amount);
+    event BalanceClaimed(address payee, uint256 amountSent);
 
     error TokenNotMintable(string metadataUri);
     error MinterNotAuthorized(string metadataUri);
     error IncorrectAmountSent(string metadataUri, uint256 amountSent);
+    error NoBalanceAvailable(address payee);
+
+    modifier hasBalance() {
+        if (balance[msg.sender] == 0) {
+            revert NoBalanceAvailable(msg.sender);
+        }
+        _;
+    }
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        commissionPayee = msg.sender;
+        defaultCommissionPercentage = 30;
     }
 
     function setNotarizedDocumentNftContact(NotarizedDocumentNft _ndn) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        ndn = _ndn;
+        notarizedDocumentNftContract = _ndn;
+    }
+
+    function setCommissionPayee(address _payee) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        // Move existing commissionPayee balance to the new payee
+        balance[_payee] += balance[commissionPayee];
+        balance[commissionPayee] = 0;
+
+        commissionPayee = _payee;
+        emit CommissionPayeeChanged(commissionPayee);
     }
 
     function createNotarizedDocument(
@@ -35,7 +61,7 @@ contract Notary is AccessControl {
         uint256 _mintingPrice,
         string memory _metadataUrl
     ) external onlyRole(NOTARY_ROLE) {
-        unmintedToken[_metadataUrl] = UnmintedTokenData(_authorizedMinter, _mintingPrice);
+        unmintedToken[_metadataUrl] = UnmintedTokenData(msg.sender, _authorizedMinter, _mintingPrice);
         emit NotarizedDocumentCreated(msg.sender, _authorizedMinter, _mintingPrice, _metadataUrl);
     }
 
@@ -50,10 +76,35 @@ contract Notary is AccessControl {
         if (tokenParameters.costToMint != msg.value) {
             revert IncorrectAmountSent(_metadataUrl, msg.value);
         }
-        tokenId = ndn.mint(msg.sender, _metadataUrl);
+        _splitPayment(msg.value, tokenParameters.notary);
+
+        tokenId = notarizedDocumentNftContract.mint(msg.sender, _metadataUrl);
         delete unmintedToken[_metadataUrl];
 
         emit NotarizedDocumentNftMinted(msg.sender, msg.value, _metadataUrl, tokenId);
+    }
+
+    function payableBalance() public view returns (uint256 _balance) {
+        _balance = balance[msg.sender];
+    }
+
+    function claim() public hasBalance {
+        uint256 amountToSend = balance[msg.sender];
+        balance[msg.sender] = 0;
+        payable(msg.sender).transfer(amountToSend);
+        emit BalanceClaimed(msg.sender, amountToSend);
+    }
+
+    function _splitPayment(uint256 amount, address notary) internal {
+        uint256 commission = amount * defaultCommissionPercentage / 100;
+        uint256 notaryPayment = amount - commission;
+        require(commission + notaryPayment == amount); // Math broke. This should catch any uncaught over/under flows
+
+        balance[notary] += notaryPayment;
+        emit BalanceIncreased(notary, notaryPayment);
+
+        balance[commissionPayee] += commission;
+        emit BalanceIncreased(commissionPayee, commission);
     }
 }
 
--- contract/test/Notary.js
+++ contract/test/Notary.js
@@ -8,10 +8,9 @@ describe("Notary contract", function () {
     it("notarizedDocumentNft should be set", async function () {
         const {notarizedDocumentNftContract, notaryContract} = await loadFixture(deployFixture)
 
-        const ndn = await notaryContract.ndn();
+        const ndn = await notaryContract.notarizedDocumentNftContract();
         expect(ndn).to.equal(notarizedDocumentNftContract.address);
     });
-
     it("admin should be able to add a notary", async () => {
         const {notaryContract, notary} = await loadFixture(deployFixture)
         const role = await notaryContract.NOTARY_ROLE()
@@ -26,14 +25,12 @@ describe("Notary contract", function () {
             await notaryContract.hasRole(role, notary.address)
         ).to.be.true
     })
-
     it("should not allow a sender who isn't a notary to create a notarized document", async () => {
         const {notaryContract, alice, mallory} = await loadFixture(deployFixture)
         await expect(
             notaryContract.connect(mallory).createNotarizedDocument(alice.address, 99, 'ipfs://bogusUri')
         ).to.be.reverted
     })
-
     it("should emit NotarizedDocumentCreated when a notarized document is created", async () => {
         const {notaryContract, alice, notary} = await loadFixture(deployFixture)
         const role = await notaryContract.NOTARY_ROLE()
@@ -45,7 +42,6 @@ describe("Notary contract", function () {
             .to.emit(notaryContract, "NotarizedDocumentCreated")
             .withArgs(notary.address, alice.address, 999, 'ipfs://bogusUri')
     })
-
     describe("minting", () => {
         const ipfsBogusUri = 'ipfs://bogusUri';
         let notaryContract, alice, notary, mallory, notarizedDocumentNftContract, role
@@ -85,9 +81,73 @@ describe("Notary contract", function () {
             const tokenId = await notaryContract.connect(alice).mint(ipfsBogusUri, {value: 99}) // FIXME A transaction is returned
             expect(await notarizedDocumentNftContract.tokenURI(tokenId)).to.equal(ipfsBogusUri)
         })
+    })
+    describe("payment", async () => {
+        const ipfsBogusUri = 'ipfs://bogusUri';
+        let notaryContract, alice, notary, mallory, notarizedDocumentNftContract, role
+
+        beforeEach(async () => {
+            (
+                {notaryContract, owner, alice, notary, mallory, notarizedDocumentNftContract} = await loadFixture(deployFixture)
+            )
+            role = await notaryContract.NOTARY_ROLE()
+            await notaryContract.grantRole(role, notary.address)
+            await notaryContract.connect(notary).createNotarizedDocument(alice.address, 100, ipfsBogusUri)
+        })
+        it("should set the commission payee to the contract owner", async () => {
+            await expect(await notaryContract.commissionPayee()).to.equal(owner.address);
+        })
         it("should fail when when the incorrect amount of eth is sent", async () => {
             await expect(notaryContract.connect(alice).mint(ipfsBogusUri, {value: 88}))
                 .to.be.revertedWithCustomError(notaryContract, "IncorrectAmountSent")
+        })
+        it("should allocate 30% of the NFT payment to the commission payee", async () => {
+            await expect(notaryContract.connect(alice).mint(ipfsBogusUri, {value: 100}))
+                .to.emit(notaryContract, "BalanceIncreased")
+                .withArgs(owner.address, 30)
+            await expect(await notaryContract.connect(owner).payableBalance()).to.equal(30)
+        })
+        it("should allocate 70% of the NFT payment to the notary", async () => {
+            await expect(notaryContract.connect(alice).mint(ipfsBogusUri, {value: 100}))
+                .to.emit(notaryContract, "BalanceIncreased")
+                .withArgs(notary.address, 70)
+            await expect(await notaryContract.connect(notary).payableBalance()).to.equal(70)
+        })
+        it("should send allocated funds when requested", async () => {
+            await notaryContract.connect(alice).mint(ipfsBogusUri, {value: 100})
+            await expect(await notaryContract.connect(owner).claim())
+                .to.changeEtherBalances([notaryContract.address, owner.address], [-30, 30])
+        })
+        it("should zero balance after funds are claimed", async () => {
+            await notaryContract.connect(alice).mint(ipfsBogusUri, {value: 100})
+            await notaryContract.connect(notary).claim()
+            await expect(await notaryContract.connect(notary).payableBalance()).to.equal(0)
+        })
+        it("should revert when there is not a balance to be claimed", async () => {
+            await expect(notaryContract.connect(mallory).claim())
+                .to.be.revertedWithCustomError(notaryContract, "NoBalanceAvailable")
+                .withArgs(mallory.address)
+        })
+        it("should transfer commission payee's balance to new commission payee when the payee is changed", async () => {
+            await notaryContract.connect(alice).mint(ipfsBogusUri, {value: 100})
+            const beforeBalanceOld = await notaryContract.connect(owner).payableBalance()
+            const beforeBalanceNew = await notaryContract.connect(notary).payableBalance()
+
+            await notaryContract.connect(owner).setCommissionPayee(notary.address)
+
+            expect(await notaryContract.connect(owner).payableBalance())
+                .to.equal(0)
+            expect(await notaryContract.connect(notary).payableBalance())
+                .to.equal(beforeBalanceOld.add(beforeBalanceNew))
+        })
+        it("should report when the commission payee is changed", async () => {
+            await expect(await notaryContract.connect(owner).setCommissionPayee(notary.address))
+                .to.emit(notaryContract, "CommissionPayeeChanged")
+                .withArgs(notary.address)
+        })
+        it("should revert when commission payee change is unauthorized", async () => {
+            await expect(notaryContract.connect(mallory).setCommissionPayee(notary.address))
+                .to.be.reverted
         })
     })
 });
