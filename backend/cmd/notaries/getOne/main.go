c9ca1603002fd4c2b8e4a741160a9b4028aec441
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-11-02 11:42:31 +0100

    Backend: Getting Order, Notary, Participant via REST API

---
+++ backend/cmd/notaries/getOne/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	_api "notarynearby/internal/api"
+	_ns "notarynearby/internal/notary"
+	_pk "notarynearby/internal/pk"
+)
+
+//Handler lambda entrypoint
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	os, err := ps.Reader.GetOne(_pk.PublicKey(request.PathParameters["notary"]))
+	if err != nil {
+		return _api.ResponseError(fmt.Errorf("Failed fetching Order: %w", err)), nil
+	}
+
+	return _api.ResponseOK(os), nil
+}
+
+var err error
+var sess *session.Session
+var ps *_ns.Service
+
+func main() {
+	sess, err = session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("REGION")),
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed starting AWS Session: %w", err))
+	}
+
+	ps, err = _ns.New(sess)
+	if err != nil {
+		panic(fmt.Errorf("failed starting Orders Service: %w", err))
+	}
+
+	lambda.Start(Handler)
+}
---
+++ backend/cmd/orders/getOne/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	_api "notarynearby/internal/api"
+	_os "notarynearby/internal/order"
+)
+
+//Handler lambda entrypoint
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	os, err := ps.Reader.GetOne(request.PathParameters["order_id"])
+	if err != nil {
+		return _api.ResponseError(fmt.Errorf("Failed fetching Order: %w", err)), nil
+	}
+
+	return _api.ResponseOK(os), nil
+}
+
+var err error
+var sess *session.Session
+var ps *_os.Service
+
+func main() {
+	sess, err = session.NewSession(&aws.Config{
+		Region: aws.String(os.Getenv("REGION")),
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed starting AWS Session: %w", err))
+	}
+
+	ps, err = _os.New(sess)
+	if err != nil {
+		panic(fmt.Errorf("failed starting Orders Service: %w", err))
+	}
+
+	lambda.Start(Handler)
+}
