9e8d79c8c997f03d7cc29bbdf98b6736f8157311
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-11-04 17:34:34 +0100

    Backend: FIX: PublicKey lowercased always (because sometimes it has random capitals)

--- backend/internal/pk/pk.go
+++ backend/internal/pk/pk.go
@@ -1,8 +1,15 @@
 package pk
 
+import "strings"
+
 //PublicKey is just a type that makes sure we use PublicKey instead of "just a string"
 //MAYBE we'll add some sort of validation here? Maybe? If this turns out to be "a real project" :D
 type PublicKey string
+
+//String interface because PK is sometimes in various capitals :facepalm:
+func (x PublicKey) String() string {
+	return strings.ToLower(string(x))
+}
 
 //Signature is a PublicKey signed with associated PrivateKey
 type Signature string
