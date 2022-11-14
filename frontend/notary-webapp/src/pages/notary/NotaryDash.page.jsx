779846b1b9890d11ecb8a93de675ef22c1523511
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-11-14 13:13:44 +0100

    Frontend: Notary orders list

--- frontend/notary-webapp/src/pages/notary/NotaryDash.page.jsx
+++ frontend/notary-webapp/src/pages/notary/NotaryDash.page.jsx
@@ -3,15 +3,78 @@ import { Button } from 'primereact'
 import { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { getAllOrders, getNotaryProfile } from '../../api'
-import ListItem from '../../components/dash/ListItem'
 import { useAuth } from '../../context'
+import { Card } from 'primereact/card';
+import { Chip } from 'primereact/chip';
+import NoOrdersImage from '../../assets/no-orders.svg'
+
+const NoOrders = ({ orders }) => {
+  if (orders && orders.length > 0) return
+
+  return (
+    <div className="flex flex-column align-items align-items-center justify-content-center mt-8 mb-8">
+      <div>
+        <img src={NoOrdersImage} alt="No Orders" />
+      </div>
+      <div className="text-color font-bold mt-3">
+        There are no Orders
+      </div>
+      <div className="text-500 mt-3">
+        Open browser in a new window and register as Participant, create order then refresh this page!
+      </div>
+    </div>
+  )
+}
+
+const List = ({ orders }) => {
+  const navigate = useNavigate()
+
+  const join = (o) => navigate('/orders/' + o.id)
+
+  if (!orders || orders.length === 0) return
+
+  return (
+    <div>
+      {
+        orders.map((o, idx) => {
+          const lastItem = orders.length === idx + 1 ? '' : 'border-bottom-1 border-600 mb-4 pb-4';
+
+          return (
+            <div key={`order-${o.id}`} className={`text-color flex align-items-center justify-content-between ${lastItem}`}>
+              <div className="flex align-items-center">
+                <i className="pi pi-file ml-4 mr-4" />
+                <div>
+                  <h3>
+                    {o.document_type}
+                  </h3>
+                  <div className="text-500">
+                    Order ID: {o.id}
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-column">
+                <div className="flex gap-2 mb-2">
+                  <Button label="Join" onClick={() => join(o)} />
+                  <Button label="Foo" className="p-button-secondary" />
+                  <Button label="Foo" className="p-button-danger" />
+                </div>
+                <div className="flex justify-content-center">
+                  <Chip label={o.status} className="bg-primary text-white" />
+                </div>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
 
 export default () => {
   const { accountAddress } = useAuth()
   const [orders, setOrders] = useState([])
   const [isLoading, setIsLoading] = useState(false)
   const [isSigned, setIsSigned] = useState(false)
-  const navigate = useNavigate()
 
   useEffect(() => {
     ;(async () => {
@@ -35,42 +98,38 @@ export default () => {
       }
     })()
   }, [accountAddress])
-  if (isLoading) {
-    return (
-      <div className="flex align-items-center justify-content-center">
-        <ProgressSpinner />
-      </div>
-    )
-  }
-  return (
-    <div className="flex flex-column justify-content-center align-items-center">
-      <h1>Notary Dashboard</h1>
 
-      {isSigned ? (
-        <>
-          {orders.length === 0 && (
-            <div className="p-4 text-center">There are no orders</div>
-          )}
-          {orders.map((o, idx) => {
-            return (
-              <ListItem
-                data={o}
-                key={o.id}
-                onClick={() => {
-                  navigate('/orders/' + o.id)
-                }}
-              />
-            )
-          })}
-        </>
-      ) : (
-        <>
-          <div className="mb-4">You are not registered as a notary yet.</div>
-          <Link to="/notary/sign-up">
-            <Button label="Sign up " />
-          </Link>
-        </>
-      )}
+  return (
+    <div className="flex flex-column align-items justify-content-center">
+      <div className="flex justify-content-between">
+        <h1 className="flex align-items-center justify-content-center">Notary Orders</h1>
+      </div>
+
+      <Card className="bg-white">
+        {isLoading && (
+          <div className="flex flex-column align-items align-items-center justify-content-center mt-8 mb-8">
+            <ProgressSpinner />
+          </div>
+        )}
+
+        {(isSigned && !isLoading) && (
+          <>
+            <NoOrders orders={orders} />
+            <List orders={orders} />
+          </>
+        )}
+
+        {(!isSigned && !isLoading) && (
+          <>
+            <div className="mb-4">
+              You are not registered as a Notary yet.
+            </div>
+            <Link to="/notary/sign-up">
+              <Button label="Sign up " />
+            </Link>
+          </>
+        )}
+      </Card>
     </div>
   )
 }
