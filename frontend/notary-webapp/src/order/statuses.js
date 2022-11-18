66114ac6d3ca7f3c4080e4ea5bf901e8bc130bdd
Jerzy Biernacki <jerzy.biernacki@gmail.com>
2022-11-18 14:17:11 +0100

    Frontend: New order status

--- frontend/notary-webapp/src/order/statuses.js
+++ frontend/notary-webapp/src/order/statuses.js
@@ -6,3 +6,4 @@ export const StatusCanceled = 'canceled';
 export const StatusNotaryJoined = 'notary-joined'
 export const StatusDocumentSigned = 'document-signed';
 export const StatusDocumentSigningConfirmed = 'document-signing-confirmed';
+export const StatusNFTMinted =  'nft-minted'
